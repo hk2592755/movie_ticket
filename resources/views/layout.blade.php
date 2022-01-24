2289d59b24e9c6c67062b17b6c272671eec89403
hk2592755 <hk2592755@gmail.com>
2022-01-24 21:14:17 +0500

    layout
    changes layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,266 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/animate.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/odometer.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/nice-select.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/jquery.animatedheadline.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
+
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" type="image/x-icon">
+</head>
+<body>
+
+    <!-- ==========Preloader========== -->
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- ==========Preloader========== -->
+    <!-- ==========Overlay========== -->
+    <div class="overlay"></div>
+    <a href="#0" class="scrollToTop">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- ==========Overlay========== -->
+
+    <!-- ==========Header-Section========== -->
+    <header class="header-section">
+        <div class="container">
+            <div class="header-wrapper">
+                <div class="logo">
+                    <a href="index.html">
+                        <img src="{{asset('assets/images/logo/logo.png')}}" alt="logo">
+                    </a>
+                </div>
+                <ul class="menu">
+                    <li>
+                        <a href="#0" class="active">Home</a>
+
+                    </li>
+                    <li>
+                        <a href="#0">movies</a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="">Action</a>
+                            </li>
+                            <li>
+                                <a href="">Horror</a>
+                            </li>
+                            <li>
+                                <a href="">Adventure</a>
+                            </li>
+                            <li>
+                                <a href="">Comedy</a>
+                            </li>
+                            <li>
+                                <a href="">English</a>
+                            </li>
+
+                        </ul>
+                    </li>
+                    {{-- <li>
+                        <a href="#0">events</a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="events.html">Events</a>
+                            </li>
+                            <li>
+                                <a href="event-details.html">Event Details</a>
+                            </li>
+                            <li>
+                                <a href="event-speaker.html">Event Speaker</a>
+                            </li>
+                            <li>
+                                <a href="event-ticket.html">Event Ticket</a>
+                            </li>
+                            <li>
+                                <a href="event-checkout.html">Event Checkout</a>
+                            </li>
+                        </ul>
+                    </li> --}}
+                    <li>
+                        <a href="#0">sports</a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="sports.html">Sports</a>
+                            </li>
+                            <li>
+                                <a href="sport-details.html">Sport Details</a>
+                            </li>
+                            <li>
+                                <a href="sports-ticket.html">Sport Ticket</a>
+                            </li>
+                            <li>
+                                <a href="sports-checkout.html">Sport Checkout</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+
+                        <ul class="">
+                            <li>
+                                <a href="about.html">About Us</a>
+                            </li>
+
+
+
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#0">blog</a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="blog.html">Blog</a>
+                            </li>
+                            <li>
+                                <a href="blog-details.html">Blog Single</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="contact.html">contact</a>
+                    </li>
+                    <li class="header-button pr-0">
+                        <a href="sign-up.html">join us</a>
+                    </li>
+                </ul>
+                <div class="header-bar d-lg-none">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+            </div>
+        </div>
+    </header>
+    <!-- ==========Header-Section========== -->
+   @yield('main')
+
+
+
+
+
+
+    <footer class="footer-section">
+        <div class="newslater-section padding-bottom">
+            <div class="container">
+                <div class="newslater-container bg_img" data-background="{{asset('assets/images/newslater/newslater-bg01.jpg')}}">
+                    <div class="newslater-wrapper">
+                        <h5 class="cate">subscribe to Boleto </h5>
+                        <h3 class="title">to get exclusive benifits</h3>
+                        <form class="newslater-form">
+                            <input type="text" placeholder="Your Email Address">
+                            <button type="submit">subscribe</button>
+                        </form>
+                        <p>We respect your privacy, so we never share your info</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="footer-top">
+                <div class="logo">
+                    <a href="index-1.html">
+                        <img src="{{asset('assets/images/footer/footer-logo.png')}}" alt="footer">
+                    </a>
+                </div>
+                <ul class="social-icons">
+                    <li>
+                        <a href="#0">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0" class="active">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <i class="fab fa-pinterest-p"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <i class="fab fa-google"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#0">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer-bottom">
+                <div class="footer-bottom-area">
+                    <div class="left">
+                        <p>Copyright © 2020.All Rights Reserved By <a href="#0">Boleto </a></p>
+                    </div>
+                    <ul class="links">
+                        <li>
+                            <a href="#0">About</a>
+                        </li>
+                        <li>
+                            <a href="#0">Terms Of Use</a>
+                        </li>
+                        <li>
+                            <a href="#0">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#0">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="#0">Feedback</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ==========Newslater-Section========== -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="{{asset('assets/js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('assets/js/modernizr-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins.js')}}"></script>
+    <script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/js/heandline.js')}}"></script>
+    <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
+    <script src="{{asset('assets/js/magnific-popup.min.js')}}"></script>
+    <script src="{{asset('assets/js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('assets/js/wow.min.js')}}"></script>
+    <script src="{{asset('assets/js/countdown.min.js')}}"></script>
+    <script src="{{asset('assets/js/odometer.min.js')}}"></script>
+    <script src="{{asset('assets/js/viewport.jquery.js')}}"></script>
+    <script src="{{asset('assets/js/nice-select.js')}}"></script>
+    <script src="{{asset('assets/js/main.js')}}"></script>
+</body>
+</html>
